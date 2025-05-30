f0a1fedd1f58f1a97d0928cf56683360cd272913
PrajjB <flamingdragonleap@gmail.com>
2025-05-30 18:58:45 +0530

    Made some changes to the frontend components Changed the styling of the Navbar and Search components.

--- Frontend/src/components/About.js
+++ Frontend/src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='container-fluid p-5'>
+    <div className='container-fluid p-5' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <h1>Inventory Management System - MERN CRUD App by Prajjwal Bhushan</h1>
     </div>
   )
--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='container-fluid p-5'>
-        <h2>Go to Products Section.</h2>
+    <div className='container-fluid p-5' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <h2>Hello and welcome to the inventory management system!</h2>
+        <p className='lead'>This is a simple inventory management system that allows you to search for products by barcode, add new products, and view all products in the inventory.</p>
+        <p className='lead'>To get started, you can use the navigation bar to search for products, add new products, or view all products in the inventory.</p>
+        <p className='lead'>Happy inventory management!</p>
     </div>
   )
 }
